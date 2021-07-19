0fa05e94158341d11dc19ea6814f1e5348546b13
ankama-ad <ankamma.bollimuntha@amnetdigital.com>
2021-07-19 22:36:42 +0530

    backend folder added

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./backend/routes/index');
+var usersRouter = require('./backend/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'backend/views'));
+app.use(express.static(path.join(__dirname, 'backend/views')));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'backend/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
